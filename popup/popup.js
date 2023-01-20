6e3b2bdbe58550b80b8a52bc4722645d471da834
jb corbin <101614021+jbc2313@users.noreply.github.com>
2023-01-20 12:45:30 -0600

    workspace tab created

--- popup/popup.js
+++ popup/popup.js
@@ -1,1 +1,16 @@
 console.log("I'm going to take a look at your bookmarks");
+
+let buttonLink = document.getElementById("linkButton");
+
+function openWorkspace() {
+    let url = chrome.runtime.getURL("workspace/workspace.html");
+    chrome.tabs.create({ url });
+    console.log("clicked button for new tab");
+}
+
+buttonLink.addEventListener("click", function () {
+
+    openWorkspace();
+
+})
+
